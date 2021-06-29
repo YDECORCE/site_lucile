971ea1e09e378badf726960897db2a04b9d8544a
YDECORCE <y.decorce@codeur.online>
2021-06-29 21:58:03 +0200

    optimisation seo

--- index.php
+++ index.php
@@ -8,6 +8,10 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
         integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
+    <meta name="description" content="Mon premier portfolio responsive utilisant la technologie php">
+  <meta name="keywords" content="HTML, CSS, php, portfolio">
+  <meta name="author" content="Yann D.">
+  
     <title>Le site de Lucile</title>
 </head>
 
